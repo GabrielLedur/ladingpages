6c29b0623bb1ce89c3b9a629a82f13f4287d74ad
Gabriel Elias Ledur <48599013+GabrielLedur@users.noreply.github.com>
2023-03-08 21:45:21 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const title = item.querySelector('.accordion-title');
+  const content = item.querySelector('.accordion-content');
+
+  title.addEventListener('click', () => {
+    content.classList.toggle('show');
+    item.classList.toggle('active');
+  });
+});
